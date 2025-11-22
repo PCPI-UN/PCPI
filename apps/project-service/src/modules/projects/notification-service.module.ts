baae6681de5c4aa2736917aa41255b7943cf3711
J-Povea21 <jpoveafernandez21@gmail.com>
2025-11-22 14:42:25 -0500

    fix: now the project-service is using environment variables instead of harcoded values

--- apps/project-service/src/modules/projects/notification-service.module.ts
+++ apps/project-service/src/modules/projects/notification-service.module.ts
@@ -4,23 +4,28 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { NotificationGrpcClient } from './infrastructure/grpc-client/notification.grpc-client';
 import { NotificationServiceAdapter } from './infrastructure/grpc-client/notification-service.adapter';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { NOTIFICATION_SERVICE_NAME, protobufPackage } from '@app/common/generated/notification';
 
 @Module({
   imports: [
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
-        name: 'NOTIFICATION_SERVICE',
-        transport: Transport.GRPC,
-        options: {
-          package: 'notification',
-          protoPath: join(process.cwd(), 'libs/common/src/protos/notification.proto'),
-          // nombre del servicio en tu docker-compose
-          url: 'notification-service:50056',
-        },
+        name: NOTIFICATION_SERVICE_NAME,
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.GRPC,
+          options: {
+            package: protobufPackage,
+            protoPath: join(process.cwd(), 'libs/common/src/protos/notification.proto'),
+            url: configService.get<string>('NOTIFICATION_SERVICE_URL'),
+          },
+        }),
+        inject: [ConfigService],
       },
     ]),
   ],
   providers: [NotificationGrpcClient, NotificationServiceAdapter],
   exports: [NotificationServiceAdapter],
 })
-export class NotificationServiceModule {}
+export class NotificationServiceModule { }
