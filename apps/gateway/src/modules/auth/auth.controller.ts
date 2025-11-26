91c7bcd4b75b8b90872601b8d95c05fd8204d543
J-Povea21 <jpoveafernandez21@gmail.com>
2025-11-26 10:58:03 -0500

    fix: now the redirct url has /app

--- apps/gateway/src/modules/auth/auth.controller.ts
+++ apps/gateway/src/modules/auth/auth.controller.ts
@@ -135,7 +135,7 @@ export class AuthController {
     });
 
     const frontendUrl = this.configService.get<string>('FRONTEND_URL') || 'http://localhost:5173';
-    response.redirect(frontendUrl);
+    response.redirect(`${frontendUrl}/app`);
   }
 
 
