7eb79f3dc56774d8b961f59b3745e96ceef0591b
Judith Isabel Perez Conde <jiconde@uninorte.edu.co>
2026-05-27 11:34:27 -0500

    [CU-86e1k02e6]: Backes-Fix-Azure-emails

--- apps/notification-service/src/infrastructure/email/azure.adapter.ts
+++ apps/notification-service/src/infrastructure/email/azure.adapter.ts
@@ -118,11 +118,11 @@ export class AzureAdapter implements EmailServicePort, OnModuleInit {
         abortSignal: AbortSignal.timeout(15_000) // 15 segundos máximo
       });
       this.logger.log('Email send initiated, waiting for completion...', { to, subject: compiledSubject });
-      const result = await poller.pollUntilDone({
-        abortSignal: AbortSignal.timeout(30_000) // 30 segundos máximo
-      });
+      //const result = await poller.pollUntilDone({
+       // abortSignal: AbortSignal.timeout(30_000) // 30 segundos máximo
+      //});
 
-      this.logger.log(`Email sent successfully. MessageId: ${result.id}`);
+      //this.logger.log(`Email sent successfully. MessageId: ${poller.id}`);
       return { success: true };
     } catch (error) {
         if ((error as any)?.statusCode === 429) {
